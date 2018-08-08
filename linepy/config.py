1004a17e76be41928221e2f353ccf057a6acbfa9
IllustedTearsZ <megadowns123@gmail.com>
2018-08-08 15:49:54 +0000

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,10 +27,10 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[480]
     APP_VER     = '8.0.2'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'Nadya'
+    SYSTEM_NAME = 'PHANTOMZ'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")

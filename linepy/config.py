69650ddb2f88cb76e638556efec9f9a1c05acb10
IllustedTearsZ <megadowns123@gmail.com>
2018-08-08 16:02:41 +0000

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,7 +27,7 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[480]
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '8.0.2'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'PHANTOMZ'
